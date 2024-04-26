fc12ae648a653c03da515f38089e59175fb92abf
Alexander Bayer <informatiker@a-bayer.name>
2024-04-26 11:35:55 +0200

    add: Component SetConnectors and added to SettingsPage

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -2,8 +2,10 @@
   <q-page padding>
     SETTINGS PAGE
     <charly-patient></charly-patient>
+    <set-connectors></set-connectors>
   </q-page>
 </template>
 <script setup>
 import CharlyPatient from 'src/components/settings/CharlyPatient.vue'
+import SetConnectors from 'src/components/settings/SetConnectors.vue'
 </script>
