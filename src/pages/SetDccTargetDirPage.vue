9797d86db578c5f00b940bfe19b64aad7f6dceba
Alexander Bayer <informatiker@a-bayer.name>
2024-05-07 13:59:54 +0200

    add: Page "SetDccTargetDirPage"

---
+++ src/pages/SetDccTargetDirPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page padding>
+    <dcc-target-dir class="q-mt-xl"></dcc-target-dir>
+  </q-page>
+</template>
+<script setup>
+import DccTargetDir from 'src/components/settings/DccTargetDir.vue'
+</script>
