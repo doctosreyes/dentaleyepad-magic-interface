265bd26f156da89fc0220743d171bc771ea88d4a
Alexander Bayer <informatiker@a-bayer.name>
2024-05-07 13:53:28 +0200

    add: dialog.showOpenDialog() to ipc-main-handlers

--- src-electron/ipc-main-handlers.js
+++ src-electron/ipc-main-handlers.js
@@ -1,5 +1,5 @@
 import { mainWindow, appTray } from './app-when-ready'
-import { ipcMain, app } from 'electron'
+import { ipcMain, app, dialog } from 'electron'
 import log from 'electron-log/main'
 import constants from '../constants.json'
 import { trayTranslations, delay } from './main-functions'
@@ -8,6 +8,10 @@ import { iconPath } from './electron-main'
 ipcMain.on('args', (ev, data) => {
   mainWindow.webContents.send('args', data)
   log.debug(`ipc-main-handlers send args: ${data}`)
+})
+
+ipcMain.on('setBounds', (ev, data) => {
+  mainWindow.setBounds(data)
 })
 
 ipcMain.on('showMainWindow', () => {
@@ -37,6 +41,16 @@ ipcMain.on('closeAppToTray', () => {
     title: constants.app.title,
     content: trayTranslations().content
   })
+})
+
+ipcMain.on('showOpenDialog', (ev, options) => {
+  log.debug(`ipc-main-handlers -> showOpenDialog options: ${JSON.stringify(options)}`)
+  dialog.showOpenDialog(options)
+    .then((result) => {
+      mainWindow.webContents.send('selectedPath', result)
+      log.debug(`ipc-main-handlers -> showOpenDialog result: ${JSON.stringify(result)}`)
+    })
+    .catch(err => log.error(err))
 })
 
 app.on('before-quit', () => {
