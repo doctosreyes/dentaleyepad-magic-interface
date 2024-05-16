348f354ba3b2f02e7ae2cb0b1fd3b1d9c5a2f521
Alexander Bayer <informatiker@a-bayer.name>
2024-05-16 08:50:59 +0200

    add: input for XnViewPath in Component XnView and added it to "SettingsPage"

---
+++ src/components/settings/XnView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <q-input v-model="pathXnView" />
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import log from 'electron-log'
+
+const pathXnView = ref(null)
+onMounted(() => {
+  window.pl.getSettingValue('paths.xnview')
+    .then((res) => {
+      pathXnView.value = res
+    }).catch(err => { log.error(err) })
+})
+
+</script>
--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,8 +1,10 @@
 <template>
   <q-page padding>
     <AutoStartSetting></AutoStartSetting>
+    <XnView></XnView>
   </q-page>
 </template>
 <script setup>
 import AutoStartSetting from '../components/settings/AutoStartSetting.vue'
+import XnView from 'src/components/settings/XnView.vue'
 </script>
