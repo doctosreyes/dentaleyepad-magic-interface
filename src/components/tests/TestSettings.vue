2f2136e2561d0a03cad8f967dc58700bd3ad4f4d
Alexander Bayer <informatiker@a-bayer.name>
2024-06-13 15:10:23 +0200

    chg: comment -> registerShortcut in TestSettings

--- src/components/tests/TestSettings.vue
+++ src/components/tests/TestSettings.vue
@@ -1,11 +1,22 @@
 <template>
+  OCR-Scan
   <q-btn @click="checkTakeScreenshot">
     TakeScreenshot
   </q-btn>
+  <!-- <q-btn @click="checkRegisterShortcut">
+    Register Shortcut
+  </q-btn> -->
 </template>
 <script setup>
 import log from 'electron-log'
+
 const checkTakeScreenshot = () => {
   log.debug('checkTakeScreenshot')
+  window.pl.send('takeScreenshot', 'TEST')
 }
+
+/* const checkRegisterShortcut = () => {
+  log.debug('checkRegisterShortcut')
+  window.pl.send('registerShortcut', 'TEST')
+} */
 </script>
