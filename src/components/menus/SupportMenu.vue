6cf6f2d681139c08177e20b56c0b8345d04cd812
Alexander Bayer <informatiker@a-bayer.name>
2024-06-13 15:08:55 +0200

    add: Folder menus @ components

---
+++ src/components/menus/SupportMenu.vue
@@ -0,0 +1,48 @@
+<template>
+    <q-menu>
+      {{ version }}
+
+      <q-list style="min-width: 30px">
+        <q-item class="q-mt-sm" to="/tests">
+          <q-item-section avatar>
+            <q-icon name="test" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Test</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-mt-sm" to="/settings">
+          <q-item-section avatar>
+            <q-icon name="settings" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Settings</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-mt-sm" to="/input">
+          <q-item-section avatar>
+            <q-icon name="input" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Input</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-mt-sm" to="/output">
+          <q-item-section avatar>
+            <q-icon name="output" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Output</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-menu>
+</template>
+
+<script setup>
+import { version } from '../../../package.json'
+
+</script>
---
+++ src/components/menus/UserMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-list>
+    <q-item v-if="updateIsAvailable" to="/update">
+      <q-item-section>
+        <div class="row">
+          <q-icon  name="update" size="xs" /><label class="q-ml-md">{{ $t('menu.update') }}</label>
+        </div>
+      </q-item-section>
+    </q-item>
+    <q-item to="/">
+      <q-item-section>
+        <div class="row">
+          <q-icon name="home" size="xs" /><label class="q-ml-md">QR Code</label>
+        </div>
+      </q-item-section>
+    </q-item>
+    <q-item to="/userSettings">
+      <q-item-section>
+        <div class="row">
+          <q-icon name="settings" size="xs" /><label class="q-ml-md">{{ $t('menu.settings') }}</label>
+        </div>
+      </q-item-section>
+    </q-item>
+    <q-item to="/remoteSupport">
+      <q-item-section>
+        <div class="row">
+          <online-support width="18px" /><label class="q-ml-md">online support</label>
+        </div>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+<script setup>
+import onlineSupport from 'src/assets/onlineSupport.vue'
+import log from 'electron-log'
+import { ref, onMounted } from 'vue'
+
+// #region UPDATE available
+const updateIsAvailable = ref(false)
+onMounted(() => {
+  window.pl.getSettingValue('updateAvailable')
+    .then((isAvailable) => {
+      updateIsAvailable.value = isAvailable
+    })
+    .catch(err => log.error(err))
+})
+// #endregion
+</script>
