ed0f184de1098efdc3309abd85c9e4af55f45dfb
Alexander Bayer <informatiker@a-bayer.name>
2024-06-19 09:24:56 +0200

    add: Component "TestMyDialog" and implemented it in "TestsPage"

---
+++ src/components/tests/TestMyDialog.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-card>
+    <q-btn @click="setDialog">Dialog</q-btn>
+  </q-card>
+</template>
+
+<script setup>
+function setDialog () {
+  console.log('TestMyDialog - setDialog')
+  const data = {
+    text: 'Dies kommt von TestMyDialog',
+    onOkCallbackID: 1
+  }
+  window.pl.send('MainLayoutDialog', data)
+}
+</script>
--- src/pages/TestsPage.vue
+++ src/pages/TestsPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page padding>
     TESTS
+    <TestMyDialog></TestMyDialog>
     <AutoUpdateTest></AutoUpdateTest>
     <TestSettings></TestSettings>
   </q-page>
 </template>
 <script setup>
+import TestMyDialog from '../components/tests/TestMyDialog.vue'
 import TestSettings from '../components/tests/TestSettings.vue'
 import AutoUpdateTest from '../components/tests/AutoUpdateTest.vue'
 
